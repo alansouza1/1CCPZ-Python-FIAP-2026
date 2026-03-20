c10f42af9efcb7e8becdd7edcaa8c688900f6a67
alansouza1 <alansouza4001@gmail.com>
2026-03-20 20:50:19 -0300

    aula 2 variaveis e operadores

---
+++ aula02-variaveis-operadores/desafios.py
@@ -0,0 +1,19 @@
+# Desafio 01
+nome = input("Digite o seu nome: ")
+
+print(f"Seja bem vindo {nome}")
+
+# Desafio 02
+dia = input("Digite o dia do seu nascimento: ")
+mes = input("Digite o mês do seu nascimento: ")
+ano = input("Digite o ano do seu nascimento: ")
+
+print(f"Sua data de nascimento é {dia}/{mes}/{ano}")
+
+# Desafio 03
+primeiro_numero = int(input("Digite um número: "))
+segundo_numero = int(input("Digite outro número: "))
+
+soma = primeiro_numero + segundo_numero
+
+print(f"{primeiro_numero} + {segundo_numero} = {soma}")
