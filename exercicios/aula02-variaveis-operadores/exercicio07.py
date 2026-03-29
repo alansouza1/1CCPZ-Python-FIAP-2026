05ce335a5c756cd5b5b846c9e9f48b62b490da1d
alansouza1 <alansouza4001@gmail.com>
2026-03-29 12:45:37 -0300

    feat: Add aula02 exercício 07

---
+++ exercicios/aula02-variaveis-operadores/exercicio07.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Digite a primeira nota: ")) * 4
+nota2 = float(input("Digite a segunda nota: ")) * 6
+
+media = (nota1 + nota2) / 2
+media_ponderada = media / 5
+
+print(f"A nota média do aluno é de {media_ponderada}")
+
