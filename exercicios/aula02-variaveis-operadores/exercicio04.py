071cc024dc6a1fbf2ae7af557945e00a4b392503
alansouza1 <alansouza4001@gmail.com>
2026-03-29 12:17:01 -0300

    feat: Add aula02 exercício 04

---
+++ exercicios/aula02-variaveis-operadores/exercicio04.py
@@ -0,0 +1,12 @@
+preco_livro = 25
+quantidade_livro = 3
+preco_caneta = 5
+quantidade_caneta = 2
+
+preco_total_livros = preco_livro * quantidade_livro
+preco_total_canetas = preco_caneta * quantidade_caneta
+
+preco_final = preco_total_livros + preco_total_canetas
+
+print(f"O valor total gasto foi de {preco_final}")
+
