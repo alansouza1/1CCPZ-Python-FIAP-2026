1fe6729a1c12917f5f6ac81216493e9f9ce01816
alansouza1 <alansouza4001@gmail.com>
2026-05-08 21:33:16 -0300

    feat: Add aula05

---
+++ exercicios/aula05-strings-array-e-matrizes/aula05-atividade01.py
@@ -0,0 +1,5 @@
+nomes = ["Ana", "João", "Léo", "Bia"]
+
+for i in range(len(nomes)):
+    for j in range((i + 1), len(nomes)):
+        print(f"{nomes[i]} + {nomes[j]}")
---
+++ exercicios/aula05-strings-array-e-matrizes/aula05-matrizes.py
@@ -0,0 +1,10 @@
+musicas = [
+    ["Three Little Birds", "Bob Marley"],
+    ["No More Tears", "Ozzy"],
+    ["hino du curintia", "curintia"]
+]
+
+for musica in musicas:
+    for info in musica:
+        print(info)
+    print()
