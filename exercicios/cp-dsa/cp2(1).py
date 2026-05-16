96d6b0b0510fd92c8777318531023774007ae151
alansouza1 <alansouza4001@gmail.com>
2026-05-15 22:46:17 -0300

    feat: Refactor exercicio

--- exercicios/cp-dsa/cp2(1).py
+++ exercicios/cp-dsa/cp2(1).py
@@ -15,16 +15,23 @@ fim = time.time()
 print(fim - inicio, "segundos")
 
 inicio = time.time()
-for i in range (0, 10**6, 4):
+for i in range (0, 10**6, 10):
     soma += i
     soma += i + 1
     soma += i + 2
     soma += i + 3
+    soma += i + 4
+    soma += i + 5
+    soma += i + 6
+    soma += i + 7
+    soma += i + 8
+    soma += i + 9
 fim = time.time()
 print(fim - inicio, "segundos")
 
-n = 10**6
+
 inicio = time.time()
+n = 10**6
 soma = n * (n - 1) / 2
 fim = time.time()
 print(fim - inicio, "segundos")
